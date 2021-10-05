e2e7596ed78fcb97f323f1b55463e9a87ddb5226
ujbalazs <tucika3@gmail.com>
2021-10-05 20:11:39 +0200

    ablak elemek felrakása

--- src/main/java/ub/ub/HelloController.java
+++ src/main/java/ub/ub/HelloController.java
@@ -2,13 +2,44 @@ package ub.ub;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label label1;
+    @FXML
+    private Label label2;
+    @FXML
+    private Label label3;
+    @FXML
+    private Label label4;
+    @FXML
+    private Label label5;
+    @FXML
+    private Label label6;
+    @FXML
+    private Label label7;
+    @FXML
+    private Label label8;
+    @FXML
+    private Label label9;
+    @FXML
+    private Label label10;
+    @FXML
+    private Label label11;
+    @FXML
+    private Label label12;
+    @FXML
+    private Label label13;
+    @FXML
+    private Label label14;
+    @FXML
+    private Label label15;
+    @FXML
+    private Label kerdes;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
     }
 }
