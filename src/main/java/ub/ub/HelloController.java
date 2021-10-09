85c953b584af0882dfbd3979eb4bda1c2f5b1188
ujbalazs <tucika3@gmail.com>
2021-10-09 17:49:32 +0200

    fxml fix, új játék gomb bekötés

--- src/main/java/ub/ub/HelloController.java
+++ src/main/java/ub/ub/HelloController.java
@@ -1,20 +1,25 @@
 package ub.ub;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
 
-import java.util.ResourceBundle;
+
+
 
 public class HelloController {
     @FXML
     private Pane menu;
     @FXML
     private Pane game;
+    @FXML
+    private void startgame(){
+        menu.setVisible(false);
+        game.setVisible(true);
+    }
+
+
+
 
 
 
@@ -22,4 +27,3 @@ public class HelloController {
 
 
 
-}
