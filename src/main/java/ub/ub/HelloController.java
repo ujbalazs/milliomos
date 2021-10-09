19fccd39cc9835c2ec89287ddebee77f9ce01dcc
ujbalazs <tucika3@gmail.com>
2021-10-09 17:22:59 +0200

    dizájn

--- src/main/java/ub/ub/HelloController.java
+++ src/main/java/ub/ub/HelloController.java
@@ -1,45 +1,25 @@
 package ub.ub;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+
+import java.util.ResourceBundle;
 
 public class HelloController {
     @FXML
-    private Label label1;
+    private Pane menu;
     @FXML
-    private Label label2;
-    @FXML
-    private Label label3;
-    @FXML
-    private Label label4;
-    @FXML
-    private Label label5;
-    @FXML
-    private Label label6;
-    @FXML
-    private Label label7;
-    @FXML
-    private Label label8;
-    @FXML
-    private Label label9;
-    @FXML
-    private Label label10;
-    @FXML
-    private Label label11;
-    @FXML
-    private Label label12;
-    @FXML
-    private Label label13;
-    @FXML
-    private Label label14;
-    @FXML
-    private Label label15;
-    @FXML
-    private Label kerdes;
+    private Pane game;
 
-    @FXML
-    protected void onHelloButtonClick() {
+
 
     }
+
+
+
 }
