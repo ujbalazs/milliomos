4f2c4c7e8ce261911a87ba2c598ef9d84b2bdc9c
ujbalazs <tucika3@gmail.com>
2021-10-30 11:35:58 +0200

    válaszok kijelölhetősége és kérdések külön classba

--- src/main/java/ub/ub/HelloController.java
+++ src/main/java/ub/ub/HelloController.java
@@ -1,6 +1,5 @@
 package ub.ub;
 
-import javafx.animation.Timeline;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.chart.*;
@@ -9,9 +8,8 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-
 import java.util.Random;
-import java.util.concurrent.TimeUnit;
+
 
 
 public class HelloController {
@@ -46,7 +44,7 @@ public class HelloController {
     @FXML
     private Button audience;
     @FXML
-    private BarChart audience_chart;
+    private BarChart<String, Number> audience_chart;
     @FXML
     private Label a_chart;
     @FXML
@@ -55,33 +53,34 @@ public class HelloController {
     private Label c_chart;
     @FXML
     private Label d_chart;
+    @FXML
+    private Label question_click;
 
 
 
 
-    // Kérdések
-    String[][] questions = {
-            {"Első kérdés?", "egy a valasz", "egy b valasz helyes", "egy c valasz", "egy d valasz", "b"},
-            {"Masodik kérdés?", "kettő a valasz", "kettő b valasz", "kettő c valasz helyes", "kettő d valasz", "c"},
-            {"Harmadik kérdés?", "harmadik a valasz", "harmadik b valasz", "harmadik c valasz", "harmadik d valasz helyes", "d"},
-            {"Negyedik kérdés?", "negyedik a valasz", "negyedik b valasz", "negyedik c valasz", "negyedik d valasz helyes", "d"},
-            {"Ötödik kérdés?", "ötödik a valasz helyes", "ötödik b valasz", "ötödik c valasz", "ötödik d valasz", "a"},
-    };
-    // Számláló amit növelek
+    Questions q = new Questions();
+
+    // Számlálók
     int counter = 0;
+    int assigment_counter_a = 0;
+    int assigment_counter_b = 0;
+    int assigment_counter_c = 0;
+    int assigment_counter_d = 0;
+
 
     Random rand = new Random();
 
     // Kérdésváltó
     public void changequestion(String id) {
 
-        if (id.equals(questions[counter][5])) {
+        if (id.equals(q.questions[counter][5])) {
             counter++;
-            k.setText(questions[counter][0]);
-            a.setText(questions[counter][1]);
-            b.setText(questions[counter][2]);
-            c.setText(questions[counter][3]);
-            d.setText(questions[counter][4]);
+            k.setText(q.questions[counter][0]);
+            a.setText(q.questions[counter][1]);
+            b.setText(q.questions[counter][2]);
+            c.setText(q.questions[counter][3]);
+            d.setText(q.questions[counter][4]);
         } else {
             close.setVisible(true);
         }
@@ -90,11 +89,11 @@ public class HelloController {
     // Gomb események
     @FXML
     private void startgame() {
-        k.setText(questions[counter][0]);
-        a.setText(questions[counter][1]);
-        b.setText(questions[counter][2]);
-        c.setText(questions[counter][3]);
-        d.setText(questions[counter][4]);
+        k.setText(q.questions[counter][0]);
+        a.setText(q.questions[counter][1]);
+        b.setText(q.questions[counter][2]);
+        c.setText(q.questions[counter][3]);
+        d.setText(q.questions[counter][4]);
         menu.setVisible(false);
         game.setVisible(true);
 
@@ -102,41 +101,106 @@ public class HelloController {
 
     @FXML
     private void a_push() {
+        assigment_counter_a++;
+        assigment_counter_b = 0;
+        assigment_counter_c = 0;
+        assigment_counter_d = 0;
+        b.setTextFill(Color.BLACK);
+        c.setTextFill(Color.BLACK);
+        d.setTextFill(Color.BLACK);
+
+        if(assigment_counter_a == 1){
+            a.setTextFill(Color.DARKORANGE);
+            question_click.setVisible(true);
+        }
+
+        if(assigment_counter_a == 2){
         changequestion(a.getId());
         b.setVisible(true);
         c.setVisible(true);
         d.setVisible(true);
         phone_pane.setVisible(false);
+        assigment_counter_a = 0;
+        question_click.setVisible(false);
+        a.setTextFill(Color.BLACK);}
     }
 
     @FXML
     private void b_push() {
+        assigment_counter_b++;
+        assigment_counter_a = 0;
+        assigment_counter_c = 0;
+        assigment_counter_d = 0;
+        a.setTextFill(Color.BLACK);
+        c.setTextFill(Color.BLACK);
+        d.setTextFill(Color.BLACK);
+
+        if(assigment_counter_b == 1){
+            question_click.setVisible(true);
+            b.setTextFill(Color.DARKORANGE);}
+
+        if(assigment_counter_b == 2){
         changequestion(b.getId());
         a.setVisible(true);
         c.setVisible(true);
         d.setVisible(true);
         phone_pane.setVisible(false);
         audience_pane.setVisible(false);
+        assigment_counter_b = 0;
+        question_click.setVisible(false);
+        b.setTextFill(Color.BLACK);}
     }
 
     @FXML
     private void c_push() {
+        assigment_counter_c++;
+        assigment_counter_a = 0;
+        assigment_counter_b = 0;
+        assigment_counter_d = 0;
+        a.setTextFill(Color.BLACK);
+        b.setTextFill(Color.BLACK);
+        d.setTextFill(Color.BLACK);
+
+        if(assigment_counter_c == 1){
+            question_click.setVisible(true);
+            c.setTextFill(Color.DARKORANGE);}
+
+        if(assigment_counter_c == 2){
         changequestion(c.getId());
         a.setVisible(true);
         b.setVisible(true);
         d.setVisible(true);
         phone_pane.setVisible(false);
         audience_pane.setVisible(false);
+        assigment_counter_c = 0;
+        question_click.setVisible(false);
+        c.setTextFill(Color.BLACK);}
     }
 
     @FXML
     private void d_push() {
+        assigment_counter_d++;
+        assigment_counter_a = 0;
+        assigment_counter_b = 0;
+        assigment_counter_c = 0;
+        a.setTextFill(Color.BLACK);
+        b.setTextFill(Color.BLACK);
+        c.setTextFill(Color.BLACK);
+
+        if(assigment_counter_d == 1){
+            question_click.setVisible(true);
+            d.setTextFill(Color.DARKORANGE);}
+
+        if(assigment_counter_d == 2){
         changequestion(d.getId());
         a.setVisible(true);
         b.setVisible(true);
         c.setVisible(true);
         phone_pane.setVisible(false);
         audience_pane.setVisible(false);
+        assigment_counter_d = 0;
+        question_click.setVisible(false);
+        d.setTextFill(Color.BLACK);}
     }
 
     @FXML
@@ -147,7 +211,7 @@ public class HelloController {
     //Felezés segítség
     @FXML
     private void half_push() {
-        String buttonid = questions[counter][5];
+        String buttonid = q.questions[counter][5];
         int randomnum = rand.nextInt(4 - 1) + 1;
         if (buttonid.equals("a")) {
             if (randomnum == 1) {
@@ -222,13 +286,11 @@ public class HelloController {
         Runnable task = () -> {
             try {
 
-                String[] answers = {"Csörög...", "Szia! Várjál, had gonodlkozzak...", "Nem vagyok benne biztos, de talán...", "Szerintem a "+questions[counter][5].toUpperCase() +" lesz a jó válasz!" };
+                String[] answers = {"Csörög...", "Szia! Várjál, had gonodlkozzak...", "Nem vagyok benne biztos, de talán...", "Szerintem a "+q.questions[counter][5].toUpperCase() +" lesz a jó válasz!" };
 
                 for (int i = 0; i < 4; i++) {
                     int index = i;
-                    Platform.runLater(() -> {
-                        dialog.setText(answers[index]);
-                    });
+                    Platform.runLater(() -> dialog.setText(answers[index]));
 
                     Thread.sleep(3000);
                 }
@@ -245,7 +307,7 @@ public class HelloController {
         audience.setTextFill(Color.RED);
         audience.setDisable(true);
 
-        XYChart.Series aud = new XYChart.Series ();
+        XYChart.Series<String, Number> aud = new XYChart.Series<>();
         aud.setName("Szavazatok");
 
         int one_random = rand.nextInt(100 - 26) + 26;
@@ -256,57 +318,57 @@ public class HelloController {
         int four_random = supp_number_two - three_random;
 
 
-        if(questions[counter][5].equals("a")){
+        if(q.questions[counter][5].equals("a")){
 
-            aud.getData().add(new XYChart.Data("", one_random));
+            aud.getData().add(new XYChart.Data<>("", one_random));
             a_chart.setText("A: "+one_random+"%");
-            aud.getData().add(new XYChart.Data(" ", two_random));
+            aud.getData().add(new XYChart.Data<>(" ", two_random));
             b_chart.setText("B: "+two_random+"%");
-            aud.getData().add(new XYChart.Data("  ", three_random));
+            aud.getData().add(new XYChart.Data<>("  ", three_random));
             c_chart.setText("C: "+three_random+"%");
-            aud.getData().add(new XYChart.Data("   ", four_random));
+            aud.getData().add(new XYChart.Data<>("   ", four_random));
             d_chart.setText("D: "+four_random+"%");
 
             audience_chart.getData().add(aud);
         }
 
-        if(questions[counter][5].equals("b")){
+        if(q.questions[counter][5].equals("b")){
 
-            aud.getData().add(new XYChart.Data("", two_random));
+            aud.getData().add(new XYChart.Data<>("", two_random));
             a_chart.setText("A: "+two_random+"%");
-            aud.getData().add(new XYChart.Data(" ", one_random));
+            aud.getData().add(new XYChart.Data<>(" ", one_random));
             b_chart.setText("B: "+one_random+"%");
-            aud.getData().add(new XYChart.Data("  ", three_random));
+            aud.getData().add(new XYChart.Data<>("  ", three_random));
             c_chart.setText("C: "+three_random+"%");
-            aud.getData().add(new XYChart.Data("   ", four_random));
+            aud.getData().add(new XYChart.Data<>("   ", four_random));
             d_chart.setText("D: "+four_random+"%");
 
             audience_chart.getData().add(aud);
         }
 
-        if(questions[counter][5].equals("c")){
+        if(q.questions[counter][5].equals("c")){
 
-            aud.getData().add(new XYChart.Data("", two_random));
+            aud.getData().add(new XYChart.Data<>("", two_random));
             a_chart.setText("A: "+two_random+"%");
-            aud.getData().add(new XYChart.Data(" ", three_random));
+            aud.getData().add(new XYChart.Data<>(" ", three_random));
             b_chart.setText("B: "+three_random+"%");
-            aud.getData().add(new XYChart.Data("  ", one_random));
+            aud.getData().add(new XYChart.Data<>("  ", one_random));
             c_chart.setText("C: "+one_random+"%");
-            aud.getData().add(new XYChart.Data("   ", four_random));
+            aud.getData().add(new XYChart.Data<>("   ", four_random));
             d_chart.setText("D: "+four_random+"%");
 
             audience_chart.getData().add(aud);
         }
 
-        if(questions[counter][5].equals("d")){
+        if(q.questions[counter][5].equals("d")){
 
-            aud.getData().add(new XYChart.Data("", two_random));
+            aud.getData().add(new XYChart.Data<>("", two_random));
             a_chart.setText("A: "+two_random+"%");
-            aud.getData().add(new XYChart.Data(" ", three_random));
+            aud.getData().add(new XYChart.Data<>(" ", three_random));
             b_chart.setText("B: "+three_random+"%");
-            aud.getData().add(new XYChart.Data("  ", four_random));
+            aud.getData().add(new XYChart.Data<>("  ", four_random));
             c_chart.setText("C: "+four_random+"%");
-            aud.getData().add(new XYChart.Data("   ", one_random));
+            aud.getData().add(new XYChart.Data<>("   ", one_random));
             d_chart.setText("D: "+one_random+"%");
 
             audience_chart.getData().add(aud);
@@ -314,6 +376,8 @@ public class HelloController {
 
 
     }
+
+
 }
 
 
---
+++ src/main/java/ub/ub/Questions.java
@@ -0,0 +1,22 @@
+package ub.ub;
+
+public class Questions {
+
+    String[][] questions = {
+            {"Miből készül a sajt?", "Tojásbol", "Tejből", "Lisztből", "Cukorból", "b"}, //1.
+            {"Melyik ország fővárosa Dublin?", "Izland", "India", "Írország", "Irak", "c"}, //2.
+            {"Ki volt Rózsa Sándor?", "Muzsikus", "Szinész", "Költő", "Betyár", "d"}, //3.
+            {"Hány vércsoport létezik?", "Nyolc", "Tíz", "Kettő", "Négy", "d"}, //4.
+            {"Hol született Petőfi Sándor?", "Kiskőrösön", "Nagykőrösön", "Nagyszalontán", "Kisszalontán", "a"}, //5,
+            {"Ki volt Gandalf, A Gyűrűk Urában?", "Mágus", "Hobbit", "Törp", "Tünde", "a"}, //6,
+            {"Milyen autómárka a Ferrari?", "Spanyol", "Olasz", "Francia", "Német", "b"}, //7.
+            {"Ki volt Szinyei Merse Pál?", "Költő", "Szobrász", "Festő", "Zenész", "c"}, //8.
+            {"Mi a kénnnek a vegyjele?", "K", "Cl", "N", "S", "d"}, //9.
+            {"Mit jelent az hogy CPU?", "Processzor", "Professzor", "Profósz", "Profi", "a"}, //10.
+            {"Milyen étel a slambuc?", "diós tészta", "Sajtos tészta", "Mákos tészta", "Krumplis tészta", "d"}, //11.
+            {"Minek a mértékegysége a newton?", "Sebesség", "Súly", "Erő", "Gyorsulás", "c"}, //12.
+            {"Ki a Csillagok háborújának atyja?", "Jake Lloyd", "George Lucas", "J. K. Rowling", " Ewan McGregor", "b"}, //13.
+            {"Milyen állat Garfield?", "Macska", "Kutya", "Ló", "Szamár", "a"}, //14.
+            {"Melyik a Föld leghosszabb folyója?", "Duna", "Mississipi", "Nílus", "Amazonas", "d"}, //15.
+    };
+}
